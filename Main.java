0706c9bb0941867a5deec1d1941fe0c79cfc81b7
marcelgs13 <119629090+marcelgs13@users.noreply.github.com>
2024-05-04 19:13:35 -0300

    Teste_funcionando-01

---
+++ Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.dequeue();
+        queue.showQueue();
+    }
+
+    public static class Queue {
+        private ArrayList<Integer> e;
+
+        public Queue() {
+            e = new ArrayList<>();
+        }
+
+        public void enqueue(int valor) {
+            e.add(valor);
+            System.out.println("Adicionado: " + valor);
+        }
+
+        public void dequeue() {
+            int i = e.remove(1);
+            System.out.println("Removido: " + i);
+        }
+
+        public void showQueue() {
+            System.out.println("Fila completa: " + e.toString());
+        }
+    }
+}
