eaae0ecfd4b11f18314f392add015dc0a395eccb
marcelgs13 <119629090+marcelgs13@users.noreply.github.com>
2024-05-06 13:05:38 -0300

    Teste_funcionando

--- Main.java
+++ Main.java
@@ -1,34 +1,36 @@
-import java.util.ArrayList;
-//Aluno:Marcel Gustavo de Barros Araujo e Marcelo Augusto de Barros Araujo
-public class Main {
-    public static void main(String[] args) {
-        Queue queue = new Queue();
-        queue.enqueue(1);
-        queue.enqueue(2);
-        queue.enqueue(3);
-        queue.dequeue();
-        queue.showQueue();
+import java.util.ArrayList; 
+
+// Aluno: Marcel Gustavo de Barros Araujo e Marcelo Augusto de Barros Araujo
+
+public class Main { 
+    public static void main(String[] args) { 
+        Queue queue = new Queue(); // Cria um objeto da classe Queue
+        queue.enqueue(1); 
+        queue.enqueue(2); 
+        queue.enqueue(3); 
+        queue.dequeue(); // Remove o primeiro elemento da fila
+        queue.showQueue(); // Exibe a fila atual
     }
 
-    public static class Queue {
-        private ArrayList<Integer> e;
+    public static class Queue { // Declaração da classe interna Queue
+        private ArrayList<Integer> e; // Declaração de uma lista privada de inteiros
 
-        public Queue() {
-            e = new ArrayList<>();
+        public Queue() { // Construtor da classe Queue
+            e = new ArrayList<>(); // Inicializa a lista de inteiros
         }
 
-        public void enqueue(int valor) {
-            e.add(valor);
-            System.out.println("Adicionado: " + valor);
+        public void enqueue(int valor) { // Método para adicionar um elemento à fila
+            e.add(valor); 
+            System.out.println("Adicionado: " + valor); 
         }
 
-        public void dequeue() {
-            int i = e.remove(1);
-            System.out.println("Removido: " + i);
+        public void dequeue() { // Método para remover um elemento da fila
+            int i = e.remove(1); 
+            System.out.println("Removido: " + i); 
         }
 
-        public void showQueue() {
-            System.out.println("Fila completa: " + e.toString());
+        public void showQueue() { // Método para exibir a fila
+            System.out.println("Fila completa: " + e.toString()); 
         }
     }
 }
